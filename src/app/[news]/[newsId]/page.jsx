874734e8eb3e542f696e9d4cc39b51b6750c5289
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-30 17:08:22 +0600

    added dynamic page

--- src/app/[news]/[newsId]/page.jsx
+++ src/app/[news]/[newsId]/page.jsx
@@ -1,11 +1,81 @@
+import { getSingleNews } from "@/utils/getSingleNews";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
 
+const NewsDetailsPage = async ({ params }) => {
+  // console.log(params)
+  const { data: news } = await getSingleNews(params.newsId);
+  // console.log(data)
+  return (
+    <Box className="my-5">
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item lg={6}>
+            <Image
+              src={news.thumbnail_url}
+              alt="news-image"
+              width={800}
+              height={500}
+            />
+            <Grid container spacing={2} className="mt-3">
+              <Grid item lg={6}>
+                <Image
+                  src={news.image_url}
+                  alt="news-image"
+                  width={800}
+                  height={500}
+                />
+              </Grid>
+              <Grid item lg={6}>
+                <Image
+                  src={news.image_url}
+                  alt="news-image"
+                  width={800}
+                  height={500}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item lg={6}>
+            <Typography variant="h5" component="h2">
+              {news.title}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 3,
+                alignItems: "center",
+                mb: "5px",
+                mt: "5px",
 
-const NewsDetailsPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+              }}
+            >
+              <Avatar alt="author" src={news.author.img} />
+              <Typography>{news.author.name}</Typography>
+              <Typography>Published {news.author.published_date}</Typography>
+            </Box>
+            <Typography
+              style={{
+                textAlign: "justify",
+                whiteSpace: "pre-line",
+                margin: "10px 0px",
+                color: 'gray'
+              }}
+            >
+              {news.details}
+            </Typography>
+            <Typography className="mt-8 text-gray-600 text-xl">``Many desktop publishing packages and web page editors now use as their default model text!</Typography>
+            <Typography>-- {news.author.name}</Typography>
+
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default NewsDetailsPage;
--- src/app/categories/[categoriesId]/page.jsx
+++ src/app/categories/[categoriesId]/page.jsx
@@ -7,6 +7,7 @@ import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
 import Image from "next/image";
 
+import Link from "next/link";
 const DynamicPage = async ({ params, searchParams }) => {
   const { data } = await getCategoryNews(searchParams.category);
   console.log(data);
@@ -25,32 +26,45 @@ const DynamicPage = async ({ params, searchParams }) => {
       >
         {data.map((news) => (
           <Grid key={news._id} item xs={6}>
-            <Card>
-              <CardActionArea>
-                <CardMedia sx={{
-                  "& img" : {
-                    width: '100%',
-                    height: '200px'
-                  }
-                }}>
-                  <Image src={news.thumbnail_url} alt="top-news" width={800} height={100} />
-                </CardMedia>
-                <CardContent>
-                  <span className="px-2 my-3 rounded  bg-red-600 text-white">
-               {news.category}
-              </span>
-                  <Typography gutterBottom variant="h-6" className="ml-2">
-                 {news.title.length >30 ? news.title.slice(0,30)+ "....": news.title}
-                  </Typography>
-                  <Typography gutterBottom className="my-3 text-sm">
-                    By {news.author.name} - {news.author. published_date}
-                  </Typography>
-                  <Typography className="text-sm">
-                {news.details.length > 200 ? news.details.slice(0,200)+"..." : news.details}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link href={`/${news.category.toLowerCase()}/${news._id}`}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    sx={{
+                      "& img": {
+                        width: "100%",
+                        height: "200px",
+                      },
+                    }}
+                  >
+                    <Image
+                      src={news.thumbnail_url}
+                      alt="top-news"
+                      width={800}
+                      height={100}
+                    />
+                  </CardMedia>
+                  <CardContent>
+                    <span className="px-2 my-3 rounded  bg-red-600 text-white">
+                      {news.category}
+                    </span>
+                    <Typography gutterBottom variant="h6">
+                      {news.title.length > 30
+                        ? news.title.slice(0, 30) + "...."
+                        : news.title}
+                    </Typography>
+                    <Typography gutterBottom className="my-3 text-sm">
+                      By {news.author.name} - {news.author.published_date}
+                    </Typography>
+                    <Typography className="text-sm">
+                      {news.details.length > 200
+                        ? news.details.slice(0, 200) + "..."
+                        : news.details}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
